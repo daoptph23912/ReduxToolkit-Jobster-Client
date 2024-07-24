39c07a583bde2c641e413b05af52ee8cfc039233
daoptph23912 <daoptph23912@fpt.edu.vn>
2024-07-24 13:22:48 +0700

    update

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -78,7 +78,7 @@ function Register() {
         <button type="submit" className="btn btn-block" disabled={isLoading}>
           {isLoading ? "loading..." : "submit"}
         </button>
-        <button
+        {/* <button
           type="button"
           className="btn btn-block btn-hipster"
           disabled={isLoading}
@@ -89,7 +89,7 @@ function Register() {
           }
         >
           {isLoading ? "loading..." : "Demo app"}
-        </button>
+        </button> */}
         <p>
           {values.isMember ? "Not a member yet?" : "Already a member?"}
           <button type="button" onClick={toggleMember} className="member-btn">
